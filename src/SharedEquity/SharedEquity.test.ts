b758663f1dfbcfc281f3be17ac4762f10120c521
Kinrany <kinrany@yandex.com>
2019-11-03 17:17:41 +0300

    Add a test for [1, 1]

--- src/SharedEquity/SharedEquity.test.ts
+++ src/SharedEquity/SharedEquity.test.ts
@@ -3,3 +3,7 @@ import { getProportions } from "./SharedEquity";
 test("getProportions returns [1] for [1]", () => {
   expect(getProportions(["1"])).toStrictEqual(["1.000"]);
 });
+
+test("getProportions returns [.5, .5] for [1, 1]", () => {
+  expect(getProportions(["1", "1"])).toStrictEqual(["0.500", "0.500"]);
+});
